2e313dec3fad1527f2391ee023300f14cf3524b0
Samuel Skean <sskean2@uic.edu>
2025-02-05 19:38:10 -0600

    Comments on type shenanigans for all_trackers.

--- src/logic.rs
+++ src/logic.rs
@@ -78,15 +78,41 @@ pub async fn tracker_events(
            WHERE trackers.id = $1",
         id
     )
+    // Some of the following code should probably be factored out into a
+    // function as we write more code that manipulates NullableEvents. It also
+    // may not be the best solution - it might be better to choose the less
+    // magical path for `collect`, below, collecting into a
+    // `Vec<Option<Event>>`. But I'm not sure.
     .fetch_all(pool)
     .await?
     .into_iter()
     .map(|ev| ev.try_into().ok())
+    // This `collect` seems kind of magical, turning an iterator that produces
+    // `Option<Events>` into a `Option<Vec<Event>>` instead of the more
+    // straightforward translation, turning it into a `Vec<Option<Event>>`
+    // (which also compiles). I assume this conversion produces None if any of
+    // the items in the Iterator are `None`, and produces some vector otherwise,
+    // but I haven't found the code that actually implements this.
+    //
+    // This is exactly what we want: we want to turn this per-row attribute of
+    // some of the fields being null into an attribute of the full result set
+    // (representing a tracker that exists, but has no events). In this case,
+    // there's only ever *one* of these rows with some nulls, and if it exists
+    // it's the only row, because we filtered on the tracker_id. We don't ensure
+    // that set of conditions here, which is the only downside to the magic I
+    // see. But I think this code is long enough. Unfortunately, there's still
+    // more shenanigans...
     .collect();
 
+    // ...Here are the shenanigans. In essence, we need to flip the case of None
+    // and the empty list.
     Ok(match empty_if_no_tracker {
+        // An empty result set means no such tracker exists:
         Some(v) if v.is_empty() => None,
+        // A non-empty result set means a tracker exists and has events:
         Some(v) => Some(v),
+        // A row with some nulls, which we turned into a None, means a tracker
+        // exists but has no events.
         None => Some(vec![]),
     })
 }
